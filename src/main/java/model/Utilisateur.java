665c72e79fc2eaa0511f9e4b6d1d981b1b5d4842
kibeti <s.kibeti@lprs.fr>
2026-05-03 16:09:56 +0200

    projet finis

---
+++ src/main/java/model/Utilisateur.java
@@ -0,0 +1,99 @@
+package model;
+
+public class Utilisateur {
+
+
+    private int id;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String motDePasse;
+
+
+    public Utilisateur(int id, String nom, String prenom, String email, String motDePasse) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.motDePasse = motDePasse;
+    }
+
+
+    public Utilisateur(String nom, String prenom, String email, String motDePasse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.motDePasse = motDePasse;
+    }
+
+    // --- Constructeur pour connexion (email + mot de passe) ---
+    public Utilisateur(String email, String motDePasse) {
+        this.email = email;
+        this.motDePasse = motDePasse;
+    }
+
+    // --- Getters & Setters ---
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    // --- toString ---
+    @Override
+    public String toString() {
+        return "Utilisateur{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+        // motDePasse volontairement exclu pour la sécurité
+    }
+
+
+    public String getRole() {
+        return "";
+    }
+
+    public String getIdUser() {
+        return "";
+    }
+
+    public void setRole(String text) {
+    }
+}
